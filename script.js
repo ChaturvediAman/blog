68f1ba72ccfa9fa5f9a1617d6c8c140dc52b4dfa
ChaturvediAman <ChaturvediAman026@gmail.com>
2021-06-24 23:30:44 +0530

    commit

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ console.log(about);
 about.addEventListener("click",()=>{
     aboutContent.style.display="flex";
 });
+
+const close=document.querySelector(".close")
+close.addEventListener("click",()=>{
+    aboutContent.style.display="none";
+});
